50c76d58db0eb661813091dba193af6ebbaf68aa
Jonathan Lau <jhylau1@gmail.com>
2014-01-05 21:10:20 -0500

    JL added hotels controller and model

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,10 +1,6 @@
 class HotelsController < ApplicationController
 	def index
-		@flight_searches = []
-		@flight_searches << FlightSearch.new(:departure_time => "2:15pm", :arrival_time => "3:15pm", :price=>"$120", :airline=>"CebuPacificAir", :duration=>"1hr")
-		@flight_searches << FlightSearch.new(:departure_time => "3:15pm", :arrival_time => "4:15pm", :price=>"$122", :airline=>"CebuPacificAir", :duration=>"1hr")
-		@flight_searches << FlightSearch.new(:departure_time => "4:15pm", :arrival_time => "5:15pm", :price=>"$126", :airline=>"CebuPacificAir", :duration=>"1hr")
-		@flight_searches << FlightSearch.new(:departure_time => "5:15pm", :arrival_time => "6:15pm", :price=>"$123", :airline=>"CebuPacificAir", :duration=>"1hr")
+		@hotels = []
 	end
 
 	def create
--- app/models/hotel_search.rb
+++ app/models/hotel_search.rb
@@ -1,4 +1,2 @@
-class FlightSearch < ActiveRecord::Base
-	attr_accessor :departure_time, :arrival_time, :price, :airline, :duration
-	attr_accessible :departure_time, :arrival_time, :price, :airline, :duration
+class HotelSearch < ActiveRecord::Base
 end
