fbead6d1c88bda1273a96fc1c46f894b48c26db1
Jonathan Lau <jhylau1@gmail.com>
2014-02-06 17:58:56 -0500

    Fixed migrations

--- db/migrate/20140123233010_add_airports.rb
+++ db/migrate/20140123233010_add_airports.rb
@@ -1,5 +1,5 @@
 class AddAirports < ActiveRecord::Migration
-    def change
+    def up
 	  	create_table :airports do |t|
 	  		t.string :icao
 	  		t.string :iata
@@ -8,5 +8,10 @@ class AddAirports < ActiveRecord::Migration
 		    t.string :city
 		    t.string :classification
 	  	end
+		Rake::Task['database:load_airports'].invoke	
+	end
+
+	def down
+		drop_table :airports
 	end
 end
